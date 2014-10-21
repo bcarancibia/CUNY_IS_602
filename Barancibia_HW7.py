7a3cba7b3fd588e5f94caf5ea446e295138bb1de
bcarancibia <bcarancibia@gmail.com>
2014-10-20 21:15:16 -0400

    Week 7 Homework

--- Barancibia_HW7.py
+++ Barancibia_HW7.py
@@ -1,5 +1,5 @@
-#Benjamin Arancibia
-#10/20/2014
+# Benjamin Arancibia
+# 10/20/2014
 #Homework Week 7
 
 '''
@@ -18,15 +18,72 @@ Homework 5 instructions:
 '''
 
 import csv
-from timeit import timeit
+import re
 import numpy as np
 from scipy.optimize import curve_fit
 
-def scipys():
-	values = []
-	with open('/Users/bcarancibia/CUNY_IS_602/brainandbody.csv', 'rb') as csvfile:
-		reader = csv.reader(csvfile, delimiter=",")
-		for row in reader:
-    		values.append(row)
-    
+
+def scipy():
+    values = []
+    with open('/Users/bcarancibia/CUNY_IS_602/brainandbody.csv', 'rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter=",")
+        for row in reader:
+            values.append(row)
+    # Quick header remover:
+    values.pop(0)
+    # Converting strings to numbers:
+    brain = []
+    body = []
+    for i in range(len(values)):
+        body.append(float(values[i][1]))
+        brain.append(float(values[i][2]))
+
+    def slope_func(x, a, b):
+        return a * x + b
+
+    popt, pcov = curve_fit(slope_func, body, brain)
+    print  "body = " + str(popt[0]) + " * brain + " + str(popt[1])
+
+
+#HOMEWORK 5 CODE
+
+def original():
+    values = []
+    with open('/Users/bcarancibia/CUNY_IS_602/brainandbody.csv', 'rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter=",")
+        for row in reader:
+            values.append(row)
     values.pop(0) #remove header
+    brain = []
+    body = []
+    for i in range(len(values)):
+        values[i][1] = float(values[i][1])
+        values[i][2] = float(values[i][2])
+    bodymean = 0
+    brainmean = 0
+
+    #means
+    for i in range(len(values)):
+        bodymean += values[i][1]
+        brainmean += values[i][2]
+    bodymean = bodymean/float(len(values))
+    brainmean = brainmean/float(len(values))
+
+    meandistance = 0
+    mse = 0
+
+    #future reference how i got formulas: http://en.wikiversity.org/wiki/Least-Squares_Method
+    for i in range(len(values)):
+        meandistance += (values[i][1] - bodymean) * (values[i][2] - brainmean)
+        mse += (values[i][1] - bodymean) * (values[i][1]-bodymean)
+    slope = meandistance/mse
+    constant = brainmean - (slope * bodymean)
+
+    print "body = " + str(slope) + " x(brain) + " + str(constant)
+
+
+if __name__ == "__main__":
+    import timeit as ti
+    print "5 iterations to test speed\n\n"
+    print "Scipy: " + str(ti.timeit("scipy()", setup="from __main__ import scipy", number=5))
+    print "Original: " + str(ti.timeit("original()", "from __main__ import original", number=5))
