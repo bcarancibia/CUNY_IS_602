8a0cdeb044c457e2afe594a7161aecca8b9aa5fd
bcarancibia <bcarancibia@gmail.com>
2014-11-12 13:22:29 -0500

    Homework 9

--- BArancibia_HW_9.py
+++ BArancibia_HW_9.py
@@ -9,48 +9,47 @@ import tkFileDialog
 import pandas as pd
 import numpy as np
 
-epa = pd.read_csv('C:\\Users\\Brett\\Documents\\Data\\epa-http.txt', 
+epa_http = pd.read_csv('/users/bcarancibia/CUNY_IS_602/Lesson9/epa_http_http.txt', 
                   delim_whitespace = True, 
                   error_bad_lines = False,
                   names = ['host', 'date', 'request', 'reply-code', 'bytes'],
-#                  parse_dates = ['date'],
                   header = None)
 
 
 
-for i in range(len(epa['bytes'])):
-    if not epa['bytes'][i].isdigit():
-       epa['bytes'][i] = 0
+for i in range(len(epa_http['bytes'])):
+    if not epa_http['bytes'][i].isdigit():
+       epa_http['bytes'][i] = 0
        
-epa['bytes'] = epa['bytes'].astype(int)
+epa_http['bytes'] = epa_http['bytes'].astype(int)
 
-for i in range(len(epa)):
-   epa['date'][i] = epa['date'][i][1:-1]
-   tmp = epa['date'][i].split(':')
-   epa['date'][i] = "Day: " + tmp[0] + " Hour: " + tmp[1]
+for i in range(len(epa_http)):
+   epa_http['date'][i] = epa_http['date'][i][1:-1]
+   tmp = epa_http['date'][i].split(':')
+   epa_http['date'][i] = "Day: " + tmp[0] + " Hour: " + tmp[1]
 
 
-epa = epa[epa['reply-code'] != 'HTTP/1.0"']
+epa_http = epa_http[epa_http['reply-code'] != 'HTTP/1.0"']
 
 
 ##### 1 #####
-hostRequestsMost = epa['host'].value_counts().index[0]
+hostrequestsmost = epa_http['host'].value_counts().index[0]
 
 
 ##### 2 #####
-sortedByBytes = epa.groupby('host').sum().sort('bytes', ascending=False)['bytes']
+sortedbytes = epa_http.groupby('host').sum().sort('bytes', ascending=False)['bytes']
 
 ##### 3 #####
 
-busiestHour = epa['date'].value_counts().index[0]
+busiestHour = epa_http['date'].value_counts().index[0]
 
 ##### 4 #####
 # get /image.gif HTTP/1.0
-gifs = epa[epa['request'].str.contains('gif')]
+gifs = epa_http[epa_http['request'].str.contains('gif')]
 gifsMost = gifs['request'].value_counts().index[0]
 
 ##### 5 #####
-replies = epa['reply-code'].unique()
+replies = epa_http['reply-code'].unique()
 repNotTwoHundo = replies[replies != '200']
 
 ##### Output #####
@@ -58,14 +57,14 @@ repNotTwoHundo = replies[replies != '200']
 print '*' *25
 print '#1'
 print 'Most requests made by: '
-print hostRequestsMost
+print hostrequestsmost
 print '*' *25
 # 2
 print '#2'
 print 'Most received bytes by: '
-print sortedByBytes.index[0]
+print sortedbytes.index[0]
 print 'Received bytes: '
-print sortedByBytes[0]
+print sortedbytes[0]
 print '*' *25
 # 3
 print '#3'
